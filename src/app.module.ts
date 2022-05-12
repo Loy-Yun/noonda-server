4ec67dc2dde8f3a11cb0c7ccfba97a2ccb3384db
Loy-Yun <yoonsoji@gmail.com>
2022-05-13 00:06:14 +0900

    set: env & database
    - dev, prod env 분리
    - aws config 추가
    - orm config 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { PerformanceModule } from './performance/performance.module';
+import { ReviewModule } from './review/review.module';
+import { ArchiveModule } from './archive/archive.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        process.env.NODE_ENV === 'production'
+          ? 'config/.env.prod'
+          : 'config/.env.dev',
+        'config/.env.all',
+      ],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),
+    UserModule,
+    PerformanceModule,
+    ReviewModule,
+    ArchiveModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
