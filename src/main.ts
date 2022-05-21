4658d5d9e7a788a8ca06d2c6fe01ea65b4716423
Loy-Yun <yoonsoji@gmail.com>
2022-05-21 14:26:01 +0900

    chore: exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerSetup } from './util/swagger';
+import { HttpExceptionFilter } from "./util/http-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   SwaggerSetup(app);
   await app.listen(3000);
 }
---
+++ src/util/http-exception.filter.ts
@@ -0,0 +1,48 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus, Logger, Inject,
+} from '@nestjs/common';
+
+/**
+ * @Catch(HttpException)
+ * HttpException 발생시
+ */
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  logger: Logger;
+
+  constructor() {
+    this.logger = new Logger();
+  }
+
+  /**
+   * @param exception 예외 객체
+   * @param host
+   */
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    this.logger.error("exception: "+ exception);
+    const res: any =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : null;
+
+    /* 클라에 전달 */
+    response.status(status).json({
+      error: {
+        code: status,
+        message: res?.message ?? exception.message
+      }
+    });
+  }
+}
